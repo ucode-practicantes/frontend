58d263cfdab36f9c1c7debb9e5bfd9d231f12b0e
Alba Lamas <alba.v.lamas@gmail.com>
2019-03-23 10:44:20 +0100

    first version of tutorial

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {speech, ScreenAction, getCurrentScreenAction} from '../app.component'
+import {speech} from '../app.component'
 
 
 @Component({
