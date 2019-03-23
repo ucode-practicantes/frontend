eaac025cb49ac045735f7acc1aa26697b129cd7c
Alba Lamas <alba.v.lamas@gmail.com>
2019-03-23 08:50:33 +0100

    fix voice

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,34 +14,30 @@ export class HomeComponent implements OnInit {
     const speech = new Speech() // will throw an exception if not browser supported
     if(speech.hasBrowserSupport()) { // returns a boolean
       console.log("speech synthesis supported")
-      speech.init().then((data) => {
+      speech.init(
+        {
+          'onVoicesLoaded': (data) => {console.log('voices', data.voices)},
+          'volume': 1,
+          'lang': 'en-GB',
+          'rate': 1,
+          'pitch': 1,
+          'voice':'Microsoft Zira Desktop - English (United States)',
+          'splitSentences': true,
+
+        }
+      ).then((data) => {
         // The "data" object contains the list of available voices and the voice synthesis params
         console.log("Speech is ready, voices are available", data)
       }).catch(e => {
           console.error("An error occured while initializing : ", e)
       })
-
-      speech.init({
-        'volume': 1,
-           'lang': 'es',
-           'rate': 1,
-           'pitch': 1,
-           'voice':'Google español',
-           'splitSentences': true,
-           'listeners': {
-               'onvoiceschanged': (voices) => {
-                   console.log("Event voiceschanged", voices)
-               }
-           }
-      })
-
       speech.speak({
         text: 'Hello, how are you today ?',
-      }).then(() => {
-        console.log("Success !")
-      }).catch(e => {
-         console.error("An error occurred :", e)
       })
+      speech.speak({
+        text: 'Hola, cómo estás hoy?',
+      })
+
 
 
     }else{
