82dfc5d0d509b17c2b4f6cb009009aeb1001435c
Alba Lamas <alba.v.lamas@gmail.com>
2019-03-23 08:03:57 +0100

    start speech

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Speech from 'speak-tts';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,42 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const speech = new Speech() // will throw an exception if not browser supported
+    if(speech.hasBrowserSupport()) { // returns a boolean
+      console.log("speech synthesis supported")
+      speech.init().then((data) => {
+        // The "data" object contains the list of available voices and the voice synthesis params
+        console.log("Speech is ready, voices are available", data)
+      }).catch(e => {
+          console.error("An error occured while initializing : ", e)
+      })
+
+      speech.init({
+        'volume': 1,
+           'lang': 'es',
+           'rate': 1,
+           'pitch': 1,
+           'voice':'Google español',
+           'splitSentences': true,
+           'listeners': {
+               'onvoiceschanged': (voices) => {
+                   console.log("Event voiceschanged", voices)
+               }
+           }
+      })
+
+      speech.speak({
+        text: 'Hello, how are you today ?',
+      }).then(() => {
+        console.log("Success !")
+      }).catch(e => {
+         console.error("An error occurred :", e)
+      })
+
+
+    }else{
+      console.log("speech synthesis supported")
+    }
   }
 
 }
