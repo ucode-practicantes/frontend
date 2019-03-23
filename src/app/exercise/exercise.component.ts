e933512275e8747961adccbf75b5fae69b894616
MrceL97 <9719marcel@gmail.com>
2019-03-23 12:22:48 +0100

    Added exercise interface.

--- src/app/exercise/exercise.component.ts
+++ src/app/exercise/exercise.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-exercise',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercise.component.scss']
 })
 export class ExerciseComponent implements OnInit {
+  hour: number = 0;
+  minute: number = 0;
+  second: number = 0;
 
-  constructor() { }
+  interval;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.start();
+  }
+
+  start() {
+    this.interval = setInterval( () => {
+      this.second += 1;
+      if (this.second == 60) {
+        this.second = 0;
+        this.minute += 1;
+        if (this.minute == 60) {
+          this.minute = 0;
+          this.hour += 1;
+          if (this.hour = 24) {
+            this.stop();
+          }
+        }
+      }
+    }, 1000);
+  }
+
+  resume() {
+    this.start();
+  }
+
+  stop() {
+    clearInterval(this.interval);
   }
 
 }
