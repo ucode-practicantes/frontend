29b8e274b4bbd935d3f4720678623e5c92316a81
MrceL97 <9719marcel@gmail.com>
2019-03-23 07:26:44 +0100

    Added basic project structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+// Components
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { InfoComponent } from './info/info.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    InfoComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
