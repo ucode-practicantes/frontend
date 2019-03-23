4219e6c961ba8d3fee6172df82a217c7de596cb7
Alba Lamas <alba.v.lamas@gmail.com>
2019-03-23 09:17:37 +0100

    make speech globalisima

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import Speech from 'speak-tts';
+import {speech} from '../../main'
 
 @Component({
   selector: 'app-home',
@@ -11,35 +11,11 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const speech = new Speech() // will throw an exception if not browser supported
     if(speech.hasBrowserSupport()) { // returns a boolean
       console.log("speech synthesis supported")
-      speech.init(
-        {
-          'onVoicesLoaded': (data) => {console.log('voices', data.voices)},
-          'volume': 1,
-          'lang': 'en-GB',
-          'rate': 1,
-          'pitch': 1,
-          'voice':'Microsoft Zira Desktop - English (United States)',
-          'splitSentences': true,
-
-        }
-      ).then((data) => {
-        // The "data" object contains the list of available voices and the voice synthesis params
-        console.log("Speech is ready, voices are available", data)
-      }).catch(e => {
-          console.error("An error occured while initializing : ", e)
-      })
       speech.speak({
-        text: 'Hello, how are you today ?',
+        text: 'Hello, welcome to XXXAPP. Do you know how to use me? Scroll up in affirmative case. Scroll down otherwise.',
       })
-      speech.speak({
-        text: 'Hola, cómo estás hoy?',
-      })
-
-
-
     }else{
       console.log("speech synthesis supported")
     }
--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,30 @@ import { environment } from './environments/environment';
 
 import 'hammerjs';
 
+import Speech from 'speak-tts';
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+export const speech = new Speech()
+speech.init(
+  {
+    'onVoicesLoaded': (data) => {console.log('voices', data.voices)},
+    'volume': 1,
+    'lang': 'en-GB',
+    'rate': 1,
+    'pitch': 1,
+    'voice':'Microsoft Zira Desktop - English (United States)',
+    'splitSentences': true,
+
+  }
+).then((data) => {
+  // The "data" object contains the list of available voices and the voice synthesis params
+  console.log("Speech is ready, voices are available", data)
+}).catch(e => {
+    console.error("An error occured while initializing : ", e)
+})
